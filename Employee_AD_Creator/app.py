a760a9fea025c525f845eaeaeb84ef67f4b7e8af
Thomas Matlock <32077179+zep1994@users.noreply.github.com>
2023-03-02 12:31:59 -0600

    Enter Name w/ TKinter

--- Employee_AD_Creator/app.py
+++ Employee_AD_Creator/app.py
@@ -2,13 +2,17 @@ from tkinter import *
 
 root = Tk()
 
+e = Entry(root, width=50)
+e.pack()
+e.insert(0, "Enter Your Name")
 
 def myClick():
-    myLabel = Label(root, text="Look, It turns Blue")
+    hello = "Hello, " + e.get()
+    myLabel = Label(root, text=hello)
     myLabel.pack()
 
 
-myButton = Button(root, text="Click Me", padx=50, pady=50, activebackground="Blue", command=myClick)  # state=DISABLED
+myButton = Button(root, text="Enter Your Name", padx=50, pady=50, activebackground="Blue", command=myClick, fg="white", bg="black")  # state=DISABLED
 myButton.pack()
 
 root.mainloop()
