f1e41b6e97323357ab116392117b600b1172c37e
Thomas Matlock <32077179+zep1994@users.noreply.github.com>
2023-03-02 12:09:48 -0600

    Start w/ grid system

--- Employee_AD_Creator/app.py
+++ Employee_AD_Creator/app.py
@@ -5,7 +5,10 @@ root = Tk()
 # Create label Widget
 myLabel = Label(root, text="Hello World")
 myLabel2 = Label(root, text="Test 2")
+
 # Show on Screen
-myLabel.grid()
+myLabel.grid(row=0, column=0)
+myLabel2.grid(row=0, column=1)
+
 
 root.mainloop()
