00e8fc4c63a783988c22627976175d4d8e4dcb3d
Thomas Matlock <32077179+zep1994@users.noreply.github.com>
2023-03-02 12:06:23 -0600

    Init w/ Tkinter (Python)

---
+++ Employee_AD_Creator/app.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+
+# Create label Widget
+myLabel = Label(root, text="Hello World")
+myLabel2 = Label(root, text="Test 2")
+# Show on Screen
+myLabel.grid()
+
+root.mainloop()
