4fc76e998de7dfcb53f12eb78ee0b7c13187236e
Thomas Matlock <32077179+zep1994@users.noreply.github.com>
2023-03-02 12:20:56 -0600

    Create Button

--- Employee_AD_Creator/app.py
+++ Employee_AD_Creator/app.py
@@ -2,13 +2,13 @@ from tkinter import *
 
 root = Tk()
 
-# Create label Widget
-myLabel = Label(root, text="Hello World")
-myLabel2 = Label(root, text="Test 2")
 
-# Show on Screen
-myLabel.grid(row=0, column=0)
-myLabel2.grid(row=0, column=1)
+def myClick():
+    myLabel = Label(root, text="Look, It turns Blue")
+    myLabel.pack()
 
+
+myButton = Button(root, text="Click Me", padx=50, pady=50, activebackground="Blue", command=myClick)  # state=DISABLED
+myButton.pack()
 
 root.mainloop()
